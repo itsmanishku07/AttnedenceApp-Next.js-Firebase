b497c15730789627db6d1336a085c51bde80f8d8
itsmanishku07 <manish0704ku@gmail.com>
2025-08-19 11:02:36 +0530

    19 Aug

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,21 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyArpeFcM9wvSXsuWxiM2QhRQPbV8HGOUBc",
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyArpeFcM9wvSXsuWxiM2QhRQPbV8HGOUBc",
+  authDomain: "qrattendance-f1f28.firebaseapp.com",
+  projectId: "qrattendance-f1f28",
+  storageBucket: "qrattendance-f1f28.firebasestorage.app",
+  messagingSenderId: "336695458653",
+  appId: "1:336695458653:web:9f54f52c9d45c6761786b4",
 };
 
 // Initialize Firebase
